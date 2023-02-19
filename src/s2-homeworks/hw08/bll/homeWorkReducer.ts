325914ff2145dbc3d124edb786b4222d20a379ab
Kravchuk Anton <Kravch-anton@mail.ru>
2023-02-19 18:58:39 +0300

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,36 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
+            if (action.payload === 'up') {
+                return state.sort(function (a, b) {
+                    if (a.name > b.name) {
+                        return 1;
+                    }
+                    if (a.name < b.name) {
+                        return -1;
+                    }
+                    return 0;
+                });
+            }
+            if (action.payload === 'down') {
+                return state.sort(function (a, b) {
+                    if (a.name > b.name) {
+                        return -1;
+                    }
+                    if (a.name < b.name) {
+                        return 1;
+                    }
+                    return 0;
+                });
+            }
             return state // need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(value => value.age > 17)// need to fix
         }
         default:
             return state
