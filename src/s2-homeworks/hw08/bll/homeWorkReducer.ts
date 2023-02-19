cca0b18d09cf575c9d036422cca701e0328888fc
Kravchuk Anton <Kravch-anton@mail.ru>
2023-02-19 19:02:26 +0300

    fix sort

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -33,7 +33,7 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
         }
         case 'check': {
 
-            return state.filter(value => value.age > 17)// need to fix
+            return state.filter(value => value.age > 17).sort((a, b) => a.age - b.age)// need to fix
         }
         default:
             return state
