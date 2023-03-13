7c5374f97ee1c88e27c21bc2d12e17cd24751f94
Kravchuk Anton <Kravch-anton@mail.ru>
2023-03-13 18:21:22 +0300

    Made 2 sliders

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,25 +1,46 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
-import { restoreState } from '../hw06/localStorage/localStorage'
+import {restoreState} from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
+import {createTheme, ThemeProvider} from "@mui/material";
+import {purple} from "@mui/material/colors";
 
 /*
 * 1 - передать значения в оба слайдера
 * 2 - дописать типы и логику функции change
 * 3 - сделать стили в соответствии с дизайном
 * */
-
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: purple[500],
+        },
+        secondary: {
+            main: '#00CC22',
+            light: '#8B8B8B'
+        },
+    },
+});
 function HW11() {
     // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-    const change = (event: any, value: any) => {
+    const change = (event: any, value: number | number []) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
+        if (Array.isArray(value)) {
+            setValue1((value[0]))
+            setValue2((value[1]))
+        }
+        if (!Array.isArray(value)) {
+            setValue1(value)
+        }
     }
 
+
     return (
+        <ThemeProvider theme={theme}>
         <div id={'hw11'}>
             <div className={s2.hwTitle}>Homework #11</div>
 
@@ -29,14 +50,20 @@ function HW11() {
                         <span id={'hw11-value'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-single-slider'}
+                            onChange={(e, value) => change(e, value)}
                             // сделать так чтоб value1 изменялось // пишет студент
-
+                            value={value1}
+                            color={"secondary"}
                         />
                     </div>
                     <div className={s.wrapper}>
                         <span id={'hw11-value-1'} className={s.number}>{value1}</span>
                         <SuperRange
                             id={'hw11-double-slider'}
+                            onChange={(e, value) => change(e, value)}
+                            value={[value1, value2]}
+                            color={"secondary"}
+
                             // сделать так чтоб value1/2 изменялось // пишет студент
 
                         />
@@ -45,6 +72,8 @@ function HW11() {
                 </div>
             </div>
         </div>
+           </ ThemeProvider>
+
     )
 }
 
